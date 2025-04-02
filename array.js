6e64885a5fe879cf5d78fc4b5e20c516c9ae909a
JJ <4843736+espin086@users.noreply.github.com>
2025-04-01 21:31:43 -0700

    feature: added push() section to arrays

--- array.js
+++ array.js
@@ -5,3 +5,29 @@ var myArrary = ["Quincy", 1]
 
 var multiArray = [["the universe", 42], ["everything", 101010]]
 
+
+//accessing data in an array with indexes
+var ourArray = [50, 60, 70];
+var ourData = ourArray[0]; //this will return 50
+
+var myArray = [50, 60, 70];
+var myData = myArray[1]; //this will return 50
+
+console.log(ourData);
+console.log(myData);
+
+//modify data in an array with indexes
+var ourArray = [18, 64, 99];
+console.log(ourArray);
+ourArray[1] = 45; //this will change the second element of the array to 45
+console.log(ourArray);
+
+//accessing multi-dimensional arrays with indexes
+var ourArray = [["the universe", 42], ["everything", 101010]];
+var ourData = ourArray[0][1]; //this will return 42
+console.log(ourData);
+
+//pushing items into an array with push(), this pushes the new item to the end of the array
+var ourArray = [["John", 23], ["cat", 2]];
+ourArray.push(["dog", 3]);
+console.log(ourArray);
