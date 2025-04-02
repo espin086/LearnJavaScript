68e5f4b492806d0f67fc324ed5d72a15e015826d
JJ <4843736+espin086@users.noreply.github.com>
2025-04-01 21:26:25 -0700

    feature: added arrays

---
+++ array.js
@@ -0,0 +1,7 @@
+var ourArray = ["John", 23]; //this is an array with two elements, a string and a number
+
+
+var myArrary = ["Quincy", 1]
+
+var multiArray = [["the universe", 42], ["everything", 101010]]
+
