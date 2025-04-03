ace133fbaa535269211141a8d63dc65dd68029d4
JJ <4843736+espin086@users.noreply.github.com>
2025-04-02 17:57:09 -0700

    feature: added variable scope eexamples to functions.js

--- functions.js
+++ functions.js
@@ -21,3 +21,37 @@ function ourFunctionWithArgs(a, b) {
 }
 
 ourFunctionWithArgs(99, 5);
+
+// global scope and functions
+var myGlobal = 10;
+
+function fun1() {
+    oopsGlobal = 5;
+    
+}
+
+function fun2() {
+    var output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += "oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+
+}
+
+fun1();
+fun2();
+
+
+//local scope and functions
+function myLocalScope() {
+    var myVar = 5;
+    console.log(myVar);
+}
+
+myLocalScope();
+
+console.log(myVar)
